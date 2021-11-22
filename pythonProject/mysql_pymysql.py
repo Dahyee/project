26f78a1d0a5ad11c4c8ae17395eb8e2feb3539c5
Dahyee <94620060+Dahyee@users.noreply.github.com>
2021-11-22 21:16:44 +0900

    Add files via upload

---
+++ pythonProject/mysql_pymysql.py
@@ -0,0 +1,27 @@
+import pymysql
+import pandas as pd
+
+# 연결할 접속 정보
+test_db = pymysql.connect(
+    user='root', 
+    passwd='970130da@@', 
+    host='127.0.0.1', 
+    db='dahyedb', 
+    charset='utf8'
+)
+
+# DB 와 상호작용하기 위한 cursor 객체 생성
+cursor = test_db.cursor(pymysql.cursors.DictCursor)
+
+# SELECT
+sql = "SELECT * FROM test_table;"
+cursor.execute(sql)
+result = cursor.fetchall()
+
+result = pd.DataFrame(result)
+print(result)
+
+#INSERT/UPDATE/DELETE
+sql = "INSERT INTO test_table(no, name) VALUES(1, 'test')"
+cursor.execute(sql)
+test_db.commit()
